ff7ea2f5342d79aa7a9de35d1dec362b35baecf8
GGDiniz <111837291+GGDiniz@users.noreply.github.com>
2024-11-27 07:51:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+// animacao navbar
+const nav = document.querySelector('.topnav')
+window.addEventListener('scroll', fixNav)
+
+function fixNav() {
+    if(window.scrollY > nav.offsetHeight + 150) {
+        nav.classList.add('active')
+    } else {
+        nav.classList.remove('active')
+    }
+}
+//slideshow
+let slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex-1].style.justify_content = "center";
+}
+function ham() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+    document.querySelector('.topnav').style.height = "80px";
+  } else {
+    x.style.display = "flex";
+    document.querySelector('.topnav').style.height = "150px";
+    x.style.fontSize = "12px";
+  }
+}
